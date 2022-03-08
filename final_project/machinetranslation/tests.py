fcfd6c9ba7b5c24b2879eeb5d226e333d4752a55
lvmcmanus <38534993+lvmcmanus@users.noreply.github.com>
2022-03-08 13:32:11 -0500

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,29 +1,18 @@
 import unittest
-from translator import english_to_french
-from translator import french_to_english
 
-class TestE2F(unittest.TestCase):
-    """English to French tests"""
-    def test1(self):
-        # Test Hello returns Bonjour
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        # Test Hello does not return Hello
-        self.assertNotEqual(english_to_french('Hello'), 'Hello')
-        # Test None returns empty string
-        self.assertNotEqual(english_to_french("None"), '')
-        # Test empty string returns empty string
-        self.assertNotEqual(english_to_french(0), 0)
+import translator
 
-class TestF2E(unittest.TestCase):
-    """French to English tests"""
-    def test1(self):
-        # Test Bonjour returns Hello
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        # Test Bonjour does not return Bonjour
-        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
-        # Test None returns empty string
-        self.assertNotEqual(french_to_english("None"), '')
-        # Test empty string returns empty string
-        self.assertNotEqual(french_to_english(0),0)
+class EnglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(translator.english_to_french(""), "") 
+        self.assertEqual(translator.english_to_french("Hello"), "Bonjour")  
+        self.assertNotEqual(translator.english_to_french(""), "Bonjour")  
+        
 
-unittest.main(
+class FrenchToEnglish(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(translator.french_to_english(""), "") 
+        self.assertEqual(translator.french_to_english("Bonjour"), "Hello")  
+        self.assertNotEqual(translator.french_to_english(""), "Hello")  
+        
+unittest.main()
